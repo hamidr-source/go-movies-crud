63df17f93bf09913b902828c9e0e23a80edd92df
hamid <hr454837@gamil.com>
2023-06-11 14:03:42 +0330

    Add update movies function

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func getMovie(w http.ResponseWriter, r *http.Request){
 	}
 }
 
-func createMovies(w http.ReadResponse, r *http.Request){
+func createMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
@@ -62,10 +62,29 @@ func createMovies(w http.ReadResponse, r *http.Request){
 	json.NewEncoder(w).Encode(movie)
 }
 
-
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	// set json content type
+	w.Header().Set("Content-type", "application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movie, range 
+	// dalete the movie with id that you've sent
+	// add a new movie - the movie that sent in the body of params
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie 
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}  
+}
 
 func main(){
-	r := mux.NewRouter
+	r := mux.NewRouter()
 
 	movies = append(movies, Movie{ID: "1", Isbn:"438227", Title: "Movie One", Director: &Director{Firstname:"Hamid", Lastname: "Reazei"} })
 	movies = append(movies, Movie{ID: "2", Isbn:"45455", Title: "Movie Two", Director: &Director{Firstname:"Amir Bahador", Lastname: "Bahadori"} })
