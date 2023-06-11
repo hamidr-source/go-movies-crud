e5b9ae568df32b3d6bdadcd9b520d31f4441d2d5
hamid <hr454837@gamil.com>
2023-06-11 13:24:14 +0330

    Add get movie function

--- main.go
+++ main.go
@@ -24,9 +24,16 @@ type Director struct{
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter,r *http.Request){
+	w.Hedear().set("content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main(){
 	r := mux.NewRouter
 
+	movies = append(movies, Movie{ID: "1", Isbn:"438227", Title: "Movie One", Director: &Director{Fisrtname:"Hamid", lastname: "Reazei"} })
+	movies = append(movies, Movie{ID: "2", Isbn:"45455", Title: "Movie Two", Director: &Director{Fisrtname:"Amir Bahador", lastname: "Bahadori"} })
 	r.HandleFunc("./movies" , getMovies).Methods("GET")
 	r.HandleFunc("./movies/{id}" , getMovies).Methods("GET")
 	r.HandleFunc("./movies" , createMovies).Methods("POST")
