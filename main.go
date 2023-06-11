95296c177efc701579012db015c65dfce5a57375
hamid <hr454837@gamil.com>
2023-06-11 13:30:10 +0330

    Add delete movies function

--- main.go
+++ main.go
@@ -25,8 +25,20 @@ type Director struct{
 var movies []Movie
 
 func getMovies(w http.ResponseWriter,r *http.Request){
-	w.Hedear().set("content-type", "application/json")
+	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
 }
 
 func main(){
